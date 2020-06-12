4dd586472abd9d6a0e9e28e0075ba450a3019526
TreySmithIII <56052484+TreySmithIII@users.noreply.github.com>
2020-06-12 17:07:01 -0500

    hello

---
+++ app.js
@@ -0,0 +1,40 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+const phraseUl = document.querySelectorAll('ul');
+const startButton = document.querySelector('.btn__reset');
+const overlay = document.getElementById('overlay');
+const main = document.querySelector('.title');
+const header = document.querySelector('.header');
+let letter = className ='letter';
+const missed = 0; 
+
+startButton.addEventListener('click' ,  () => {
+    overlay.style.visibility = 'hidden';
+
+});
+
+
+const phrases = ['Bald ' , 'Cutie Pie ', 'BodyGuard' , 'Covered in money ', 'Bald Remix']; 
+
+function getRandomPhraseAsArray (arr) {
+let split = [];
+for (var i=0; i<arr.length; i++) {
+    return split += arr[Math.floor(Math.random()*arr.length)].split('');
+}
+
+
+};
+
+function addPhrasetoDisplay (arr) {
+    for (let i = 0; phrases.length; ++i) {
+        let li = document.createElement('li');
+        phraseUl.appendChild(li);
+    }
+
+    }
+
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    addPhrasetoDisplay(phraseArray); 
+    
+
+
