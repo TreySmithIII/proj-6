d1e07192b558d76346f75c2ff48db9c5b41c53dd
TreySmithIII <56052484+TreySmithIII@users.noreply.github.com>
2020-06-13 18:28:11 -0500

    push

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const overlay = document.getElementById('overlay');
 const main = document.querySelector('.title');
 const header = document.querySelector('.header');
 
-let letter = className ='letter';
+
 
 const missed = 0; 
 
@@ -18,27 +18,60 @@ startButton.addEventListener('click' ,  () => {
 });
 
 
-const phrases = ['Bald ' , 'Cutie Pie ', 'BodyGuard' , 'Covered in money ', 'Bald Remix']; 
+const phrases = ['Bald ' , 'Cutie Pie ', 'Body Guard' , 'Covered in money ', 'Bald Remix']; 
 
 function getRandomPhraseAsArray (arr) {
-let split = [];
-for (var i=0; i<arr.length; i++) {
-    return split += arr[Math.floor(Math.random()*arr.length)].split('');
+  let split = [];
+  for (var i=0; i<arr.length; i++) {
+      return split += arr[Math.floor(Math.random()*arr.length)];
+  }
 }
-
-
-};
-
 function addPhrasetoDisplay (arr) {
     for (let i = 0; i < arr.length; i++) {
         let li = document.createElement('li');
-        return phraseUl.appendChild(li);
+        li.textContent=arr[i];
+        phraseUl.appendChild(li);
+        if (arr[i] != ' ') {
+            li.className='letter';
+
+        } else {
+
+          li.className = 'space';
+        }
     }
 
     }
-
-    const phraseArray = getRandomPhraseAsArray(phrases);
-    addPhrasetoDisplay(phraseArray); 
     
 
+qwerty.addEventListener('click' , (e)=> {
+ 
+ if (e.target.tagName === 'BUTTON') {
+  const clicked = event.target;
+  clicked.className='chosen';
+   clicked.disabled= true;
+ }
+const checkLetter = function checkLetter (button) {
+  let letter = document.querySelectorAll('.letter');
+let matchingLetter = null;
+       for (let i = 0; i< letter.length; i++) {
+         
+          if ( button === letter[i].textContent) {
+          letter[i].className = 'letter show';
+          matchingLetter=letter[i].textContent;
+       } 
+          
+
+      return  matchingLetter;
+
+
+    } 
+  }
+
+      
+
+  
+
+});
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhrasetoDisplay(phraseArray); 
 
